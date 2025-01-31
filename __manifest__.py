5b2b4b89e180d7dd30eca029b86af9b11f1dbdb7
Khanstore <dmashraf@gmail.com>
2025-01-31 10:48:35 +0600

    branch 15 imported

--- __manifest__.py
+++ __manifest__.py
@@ -1,37 +1,39 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+# Copyright (C) 2011 Smartmode LTD (<http://www.smartmode.co.BD>).
+
 {
-    'name': 'Bangladesh - Accounting',
-    'version': '16.0.1.0.0',
+    'name': 'BD - Accounting',
+    'version': '1.0',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """
-This is the latest basic Bangladeshi localisation necessary to run Odoo in Bangladesh:
-======================================================================================
-
-This module consists of:
- - Generic Bangladesh Chart of Accounts
- - Taxes and tax report
- - Multiple Fiscal positions
- """,
-    'website': 'http://www.khan-store.com/shop',
-    'depends': ['l10n_multilang'],
+This is the latest BD Odoo localisation necessary to run Odoo accounting for BD SME's with:
+=================================================================================================
+    - a CT600-ready chart of accounts
+    - VAT100-ready tax structure
+    - InfoLogic BD counties listing
+    - a few other adaptations""",
+    'author': 'SmartMode LTD',
+    'website': 'https://www.odoo.com/page/accounting',
+    'depends': [
+        'account',
+        'base_iban',
+        'base_vat',
+    ],
     'data': [
-        # 'data/data_account_type.xml',
-        'data/account.account.tag.csv',
-        'data/account_chart_template_data.xml',
-        'data/account_tax_group_data.xml',
-
+        'data/payment.icon.csv',
+        # 'data/res.lang.csv',
+        'data/l10n_bd_chart_data.xml',
         'data/account.account.template.csv',
-        'data/account.group.template.csv',
-        'data/account_tax_report_data.xml',
-        'data/account_tax_template_data.xml',
-        'data/fiscal_templates_data.xml',
-        'data/account_chart_template_post_data.xml',
-        'data/account_chart_template_configure_data.xml',
+        'data/account.chart.template.csv',
+        'data/account.tax.group.csv',
+        # 'data/account_tax_report_data.xml',
+        # 'data/account_tax_data.xml',
+        'data/account_chart_template_data.xml',
     ],
-    'demo': [
-        'demo/demo_company.xml',
-    ],
-    'license': 'LGPL-3',
+    # 'demo': [
+    #     'demo/l10n_bd_demo.xml',
+    #     'demo/demo_company.xml',
+    # ],
 }
