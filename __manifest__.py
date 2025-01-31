45d48221e5454d82bb5a29ee8085b49d8b21cd8c
Khanstore <dmashraf@gmail.com>
2025-01-31 11:00:52 +0600

    Update __manifest__.py
    version format changed

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     'website': 'https://www.odoo.com/documentation/17.0/applications/finance/fiscal_localizations.html',
     'icon': '/account/static/description/l10n.png',
     'countries': ['bd'],
-    'version': '1.0',
+    'version': '18.0.1.0.0',
     'category': 'Accounting/Localizations/Account Charts',
     'description': ' This is the base module to manage chart of accounts and localization for the Bangladesh ',
     'depends': [
