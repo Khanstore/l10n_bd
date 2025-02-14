8ac46c60c68fa5b4cd421e744f338853d3f9660c
Khanstore <dmashraf@gmail.com>
2025-02-14 23:19:25 +0600

    perfect working in 18

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,10 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Bangladesh - Accounting',
-    'website': 'https://www.odoo.com/documentation/17.0/applications/finance/fiscal_localizations.html',
+    'name': 'Bangladesh - Accounting 20250208',
+    'website': 'https://www.odoo.com/documentation/18.0/applications/finance/fiscal_localizations.html',
     'icon': '/account/static/description/l10n.png',
     'countries': ['bd'],
-    'version': '18.0.1.0.0',
+    'version': '1.0',
     'category': 'Accounting/Localizations/Account Charts',
     'description': ' This is the base module to manage chart of accounts and localization for the Bangladesh ',
     'depends': [
@@ -13,7 +13,7 @@
     'auto_install': ['account'],
     'data': [
         'data/account.account.tag.csv',
-        'data/res.country.state.csv',
+        # 'data/res.country.state.csv',
         'data/account_tax_report_data.xml',
         'views/menu_items.xml',
     ],
